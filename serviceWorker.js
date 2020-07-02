93ea71d6c8caacd4b40c20d060161156b5d753e8
lnunez99 <59833902+lnunez99@users.noreply.github.com>
2020-07-01 20:09:56 -0500

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,22 +1,17 @@
-// A script runs in the background and can intercept & handle network requests. manage the cache, or send push notifications.
 
-const bigProject = "bigProject-v1"
+const personalCounter = "personalCounter-v1"
 // Assets stored in the cache.
 const assets = [
-  "/it202-big-project/",
-  "/it202-big-project/index.html",
-  "/it202-big-project/css-style.css",
+  "/customer-counter/",
+  "/customer-counter/index.html",
   "https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.js",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
-	"/it202-big-project/js-app.js",
-	"/it202-big-project/manifest.json",
-	"/it202-big-project/images/BPICON.png"
+	"/customer-counter/js-app.js",
+	"/customer-counter/manifest.json",
+	"/customer-counter/images/BPICON.png"
 ]
 
-// Self refers to the service worker. Enables user to listen to life cycle events and do something in return.
 // Install event gets triggered as soon as the worker exectutes, and is called only oncer per SW.
-// 
-// Because caching something on the browser takes time, waitUntil() is used.
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
 	  // When the cache API is ready, we can run the open() method and create the cache.
